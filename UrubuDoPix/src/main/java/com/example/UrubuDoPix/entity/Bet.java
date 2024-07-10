66066b2d8560acacfea3d96479c27c3e7b4b8de8
Paulo Henrique Teixeira Pompeu <paulo.pompeu@youxgroup.com.br>
2024-07-10 08:52:03 -0300

    Initial commit

---
+++ UrubuDoPix/src/main/java/com/example/UrubuDoPix/entity/Bet.java
@@ -0,0 +1,38 @@
+package com.example.UrubuDoPix.entity;
+
+import com.example.UrubuDoPix.enums.TipoBet;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(schema = "pompeu_bet", name = "bet")
+public class Bet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "valor")
+    private Double valor;
+
+    @Column(name = "tipo_bet")
+    @Enumerated(EnumType.STRING)
+    private TipoBet tipoBet;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id", nullable = false)
+    private Usuario usuario;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_evento", referencedColumnName = "id", nullable = false)
+    private Evento evento;
+
+}
